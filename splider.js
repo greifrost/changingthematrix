7c4dee80ba192f973a4be0c42842e0ee9cdcf0dc
roysan0079 <70557844+roysan0079@users.noreply.github.com>
2023-11-12 23:55:34 +0800

    Update splider.js

--- splider.js
+++ splider.js
@@ -1,18 +1,16 @@
 document.addEventListener( 'DOMContentLoaded', function() {
-var splide = new Splide( '.splide' );
 
-// let perPage = 3;
-// let width = window.innerWidth;
-// if (width < 600) {
-//     perPage = 1;
-// }
-// else if (width < 900) {
-//     perPage = 2;
-// }
-// new Splide('.splide', {
-//     type: 'loop',
-//     perPage: perPage,
-//}).mount();
+let perPage = 3;
+let width = window.innerWidth;
+if (width < 600) {
+    perPage = 1;
+}
+else if (width < 900) {
+    perPage = 2;
+}
+new Splide('.splide', {
+    type: 'loop',
+    perPage: perPage,
+}).mount();
       
-splide.mount();
 } );
